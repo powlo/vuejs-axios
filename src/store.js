7cf6a0f9ed5d41b94d7b6ca350d41bc58b449871
Paul Backhouse <paulcbackhouse@gmail.com>
2018-07-01 14:11:21 +0000

    Auto login mechanism.

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+/* global localStorage */
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axiosAuth from './axios-auth'
@@ -43,26 +45,54 @@ export default new Vuex.Store({
           returnSecureToken: true
         })
           .then(res => {
+            const now = new Date();
+            const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000);
             commit('AUTH_USER', {idToken: res.data.idToken, userId: res.data.localId});
-            dispatch('storeUser', authData)
+            localStorage.setItem('token', res.data.idToken);
+            localStorage.setItem('expires', expirationDate);
+            localStorage.setItem('id', res.data.localId);
+            dispatch('storeUser', authData);
           })
           .catch(error => console.log(error));
     },
     signIn ({commit, dispatch}, authData) {
-      console.log('signing in')
+      console.log('signing in');
+      
       axiosAuth.post(`/verifyPassword?key=${settings.api_key}`, {
         email: authData.email,
         password: authData.password,
         returnSecureToken: true
       })
         .then(res => {
+          const now = new Date();
+          const expirationDate = new Date(now.getTime() + res.data.expiresIn * 1000);
+          commit('AUTH_USER', {idToken: res.data.idToken, userId: res.data.localId});
+          localStorage.setItem('token', res.data.idToken);
+          localStorage.setItem('expires', expirationDate);
+          localStorage.setItem('id', res.data.localId);        
+
           commit('AUTH_USER', {idToken: res.data.idToken, userId: res.data.localId});
           dispatch('setSignoutTimer', res.data.expiresIn);
         })
         .catch(error => console.log(error));
     },
+    tryAutoLogin ({commit}) {
+      console.log('trying to autologin')
+      const token = localStorage.getItem('token');
+      const expiresIn = localStorage.getItem('expires');
+      const id = localStorage.getItem('id');
+      if (!token) return;
+      const now = new Date();
+      console.log('all ok');
+      if (expiresIn < now) return;
+      commit('AUTH_USER', {idToken: token, userId: id});
+      router.replace('/dashboard');
+    },
     signOut ({commit}) {
       commit('CLEAR_USER');
+      localStorage.removeItem('token');
+      localStorage.removeItem('id');
+      localStorage.removeItem('expires');
       router.replace('/signin');
     },
     storeUser ({commit, state}, user) {
